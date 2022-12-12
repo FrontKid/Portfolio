7ce549680414773ce9c0108737fb1a591139cea8
Denys Poltorak <itwfrontend@gmail.com>
2022-12-12 21:13:28 +0200

    footer was added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footerContainer} container`}>
+        <h1 className={styles.footerTitle}>Poltorak Denys</h1>
+
+        <ul className={styles.footerList}>
+          <li>
+            <a className={styles.footerLink} href="#about">About</a>
+          </li>
+
+          <li>
+            <a className={styles.footerLink} href="#portfolio">Projects</a>
+          </li>
+
+          <li>
+            <a className={styles.footerLink} href="#skills">Skills</a>
+          </li>
+        </ul>
+
+        <div className={styles.footerSocials}>
+          <a href="https://t.me/anywlhere" className={`${styles.footerSocialLink}`} target="__blank">
+            <i className="bx bxl-facebook" />
+          </a>
+          <a href="https://github.com/FrontKid" className={`${styles.footerSocialLink}`} target="__blank">
+            <i className="bx bxl-instagram" />
+          </a>
+          <a href="https://www.linkedin.com/in/denys-poltorak-6bb51824b/" className={`${styles.footerSocialLink}`} target="__blank">
+            <i className="bx bxl-twitter" />
+          </a>
+        </div>
+        <span className={styles.footerCopy}>
+          &#169; Poltorak. All rights reserved
+        </span>
+      </div>
+    </footer>
+
+  )
+}
+
+export default Footer
