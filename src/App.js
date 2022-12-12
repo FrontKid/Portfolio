04ea2e98ec8f9e02e9893afff8b70d5ca7a72b64
Denys Poltorak <itwfrontend@gmail.com>
2022-12-12 21:13:07 +0200

    footer was added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/home/Home';
 import About from './components/about/About'
 import Skills from './components/skills/Skills';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 import './App.scss';
 
@@ -15,6 +16,7 @@ function App() {
         <About />
         <Skills />
         <Contact />
+        <Footer />
       </main>
     </>
   );
